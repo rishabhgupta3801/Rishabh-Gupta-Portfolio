c2a1dace5f85e0bb4a1e60204851c0610afda4d4
Rishabh Gupta <rishabh3801@gmail.com>
2022-03-18 20:28:28 +0530

    first commit

---
+++ js/resp.js
@@ -0,0 +1,46 @@
+burger = document.querySelector('.burger');
+burger0 = document.querySelector('.burger0');
+burger1 = document.querySelector('.burger1');
+burger2 = document.querySelector('.burger2');
+burger3 = document.querySelector('.burger3');
+burger4 = document.querySelector('.burger4');
+navbar = document.querySelector('.navbar');
+rightNav = document.querySelector('.rightNav');
+leftNav = document.querySelector('.leftNav');
+
+burger.addEventListener('click', ()=>{
+    leftNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+burger0.addEventListener('click', ()=>{
+    leftNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+burger1.addEventListener('click', ()=>{
+    leftNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+});
+burger2.addEventListener('click', ()=>{
+    leftNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+});
+burger3.addEventListener('click', ()=>{
+    leftNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+});
+burger4.addEventListener('click', ()=>{
+    leftNav.classList.toggle('v-class-resp');
+    rightNav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+});
