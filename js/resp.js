20892fb30ea932cad35e35380a3875e3eca23f30
Rishabh Gupta <rishabh3801@gmail.com>
2022-04-07 08:16:04 +0530

    Issues rectified

--- js/resp.js
+++ js/resp.js
@@ -5,43 +5,45 @@ burger2 = document.querySelector('.burger2');
 burger3 = document.querySelector('.burger3');
 burger4 = document.querySelector('.burger4');
 navbar = document.querySelector('.navbar');
-rightNav = document.querySelector('.rightNav');
 leftNav = document.querySelector('.leftNav');
 
 burger.addEventListener('click', ()=>{
     leftNav.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
     navbar.classList.toggle('icon');
 })
 
 burger0.addEventListener('click', ()=>{
     leftNav.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
+    navbar.classList.toggle('icon');
+    burger.addEventListener();
 })
 
 burger1.addEventListener('click', ()=>{
     leftNav.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-
+    navbar.classList.toggle('icon');
+    burger.addEventListener();
 });
+
 burger2.addEventListener('click', ()=>{
     leftNav.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-
+    navbar.classList.toggle('icon');
+    burger.addEventListener();
 });
+
 burger3.addEventListener('click', ()=>{
     leftNav.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-
+    navbar.classList.toggle('icon');
+    burger.addEventListener();
 });
+
 burger4.addEventListener('click', ()=>{
     leftNav.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-
+    navbar.classList.toggle('icon');
+    burger.addEventListener();
 });
